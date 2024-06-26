bebce52ac2288f8bf0a891a391f16788633e7997
Eu <nap-o@users.noreply.github.com>
2024-06-26 02:31:54 -0300

    nova notícia - newsletter

--- newsletter_sbefm.zsh
+++ newsletter_sbefm.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-$("/Users/ns/Desktop/sbefm/")
 echo "Rodando script para atualizar o site da sbefm no github pages"
 
-$(git add .)
-$(git commit -m "nova notícia e novo sócio")
-$(git push origin gh-pages)
+cd Desktop/sbefm/sbefm
+git add .
+git commit -m "nova notícia - newsletter"
+git push origin gh-pages
